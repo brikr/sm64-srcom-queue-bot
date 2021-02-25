332b7b747ccba955bfe5df7bbc24a8b31c86a046
Brik Royster <brikr@users.noreply.github.com>
2021-02-25 01:09:15 -0700

    Update src/environment/environment.sample.ts

--- src/environment/environment.sample.ts
+++ src/environment/environment.sample.ts
@@ -1,9 +1,9 @@
 // Fill in the values here with your own webhook creds, and copy this file to environment.ts
 export const environment = {
   srcomApiKey: 'A SPEEDRUN.COM API KEY',
-  webhookChannelId: 'A DISCORD CHANNEL ID',
-  webhookSecret: 'A DISCORD WEBHOOK SECRET',
   dev: true,
+  dailyStatsWebhookChannelId: 'A DISCORD CHANNEL ID',
+  dailyStatsWebhookSecret: 'A DISCORD WEBHOOK SECRET',
   rejectedRunsWebhookChannelId: 'A DISCORD CHANNEL ID',
   rejectedRunsWebhookSecret: 'A DISCORD WEBHOOK SECRET',
 };
