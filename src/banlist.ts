7eab1f509771cb8e92c16fc24a1f93469c515ba6
TDDwarf <94898698+TDDwarf@users.noreply.github.com>
2021-11-23 09:34:33 +0000

    Update banlist.ts

--- src/banlist.ts
+++ src/banlist.ts
@@ -2,4 +2,5 @@
 export const BANLIST = [
   'Pipipopo', // Reason: spam run https://www.speedrun.com/sm64/run/zgr5wk0m
   'Terry123wer1', // Reason: spam run https://www.speedrun.com/sm64/run/mk560elm
+  'GothicLogic', // Reason: from Newcastle
 ];
