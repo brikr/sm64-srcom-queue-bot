d1e74bd41039f5bc2097a5301e1ac80672c07ce0
Brik <elkemore@gmail.com>
2021-11-23 15:01:46 -0700

    Update banlist.ts

--- src/banlist.ts
+++ src/banlist.ts
@@ -2,4 +2,6 @@
 export const BANLIST = [
   'Pipipopo', // Reason: spam run https://www.speedrun.com/sm64/run/zgr5wk0m
   'Terry123wer1', // Reason: spam run https://www.speedrun.com/sm64/run/mk560elm
+  'jk_kyle', // Reason: fake run https://www.speedrun.com/sm64/run/y8en5gdy
+  'Issa_the_shadow_cat', // Reason: fake run https://www.speedrun.com/sm64/run/y8eljexy
 ];
