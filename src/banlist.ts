3aa5265fba426a3aa47b391d364f092a1247ff03
thags15 <92997613+thags15@users.noreply.github.com>
2021-11-30 12:12:23 -0500

    Update banlist.ts (#40)

--- src/banlist.ts
+++ src/banlist.ts
@@ -4,4 +4,5 @@ export const BANLIST = [
   'Terry123wer1', // Reason: spam run https://www.speedrun.com/sm64/run/mk560elm
   'jk_kyle', // Reason: fake run https://www.speedrun.com/sm64/run/y8en5gdy
   'Issa_the_shadow_cat', // Reason: fake run https://www.speedrun.com/sm64/run/y8eljexy
+  'Tenden', // Reason: spam run https://www.speedrun.com/sm64/run/yjxj4lnz 
 ];
