ad39a0f1f4d9c863dbd1784f67d18bdff66be6bc
Franck Michea <franck.michea@gmail.com>
2026-02-20 16:53:43 +0100

    Add Frozican to banlist for spliced and stolen runs

--- src/banlist.ts
+++ src/banlist.ts
@@ -90,4 +90,5 @@ export const BANLIST = [
   'JacobWestcott', // Reason: spam https://www.speedrun.com/sm64/runs/zpx3n9gm
   'Culinatore', // Reason: submitted old kanno run https://www.speedrun.com/sm64/runs/yv7onooy
   'Gatorbox', // Reason: troll https://www.speedrun.com/sm64/runs/y6jo24pm
+  'Frozican', // Reason: submitted spliced runs, stolen runs and TAS runs to main leaderboards
 ];
